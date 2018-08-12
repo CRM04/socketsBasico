5080d1cf49803f770d8d8897888fc19ad449701a
CRM04 <kriz-azul-95@hotmail.com>
2018-08-11 21:42:36 -0500

    actualizacion de sockets

--- server/server.js
+++ server/server.js
@@ -1,20 +1,24 @@
 const express = require('express');
-
+const socketIO = require('socket.io');
 const path = require('path');
+const http = require('http');
+const chalk = require('chalk');
 
 const app = express();
-
+let server = http.createServer(app);
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 app.use(express.static(publicPath));
 
 
+//mantiene la conxecion del backend
+module.exports. io = socketIO(server);
+require('./sockets/socket')
 
-app.listen(port, (err) => {
+server.listen(port, (err) => {
 
     if (err) throw new Error(err);
 
-    console.log(`Servidor corriendo en puerto ${ port }`);
+    console.log(chalk.blue(`SERVIDOR CORRIENDO EN PUERTO ${ port }`));
 
 });
